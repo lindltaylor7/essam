9fba45f7b76c2251e1a69e158fd620440b8df682
digitechgrouperu <digitechgrouperu@gmail.com>
2025-03-12 19:56:23 -0500

    test interconnection

--- app/page.js
+++ app/page.js
@@ -11,7 +11,7 @@ export default function Home() {
   const onSubmit = async(data) => {
     try {
       // Enviar los datos del formulario al servidor en el puerto 4000
-      const response = await axios.post('http://localhost:4000/login', data);
+      const response = await axios.post('http://essam.fun:4000/login', data);
       
       // Si la respuesta es exitosa, redirigir a /organizations
       if (response.status === 200) {
