161828b965a78477593e3d32d6dcedb33eaebde8
digitechgrouperu <digitechgrouperu@gmail.com>
2025-03-18 16:24:55 -0500

    update frontend

--- app/page.js
+++ app/page.js
@@ -36,7 +36,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <h1 className="text-3xl sm:text-4xl text-center sm:text-left">
-          Control de Comensales
+          ERP
         </h1>
           <form onSubmit={handleSubmit(onSubmit)} className="rounded p-5 bg-zinc-800 flex flex-col w-full" >
             <input type="text" className="p-2 my-2 bg-zinc-500 rounded" placeholder="Email" {...register("email")}/>
