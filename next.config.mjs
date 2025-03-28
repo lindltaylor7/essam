1149ab5476f6637c64455f62dfc36e968d9ad9ab
digitechgrouperu <digitechgrouperu@gmail.com>
2025-03-28 12:05:01 -0500

    fix sales

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    // Habilita polyfills para módulos de Node.js
+    webpack: (config, { isServer }) => {
+      if (!isServer) {
+        config.resolve.fallback = {
+          ...config.resolve.fallback,
+          crypto: require.resolve("crypto-browserify"),
+          stream: require.resolve("stream-browserify"),
+          zlib: require.resolve("browserify-zlib"),
+          http: require.resolve("stream-http"),
+          https: require.resolve("https-browserify"),
+          os: require.resolve("os-browserify/browser"),
+          path: require.resolve("path-browserify"),
+        };
+      }
+      return config;
+    },
+    // Opcional: Si usas `@react-pdf/renderer` solo en el cliente
     serverExternalPackages: ['@react-pdf/renderer'],
-};
-
-export default nextConfig;
+  };
+  
+  export default nextConfig;
