ad0d553cab086c96e2a3a79278e83c504bcde353
digitechgrouperu <digitechgrouperu@gmail.com>
2025-03-28 12:11:23 -0500

    fix sales

--- next.config.mjs
+++ next.config.mjs
@@ -3,12 +3,22 @@ const nextConfig = {
     webpack: (config, { isServer }) => {
       if (!isServer) {
         config.resolve.fallback = {
-          crypto: require.resolve("crypto-browserify"),
-          stream: require.resolve("stream-browserify"),
+          crypto: false, // Usamos false en lugar del polyfill
+          stream: false,
+          zlib: false,
+          http: false,
+          https: false,
+          os: false,
+          path: false,
         };
       }
       return config;
     },
+    serverExternalPackages: ['@react-pdf/renderer'],
+    // Solución alternativa para React 19:
+    experimental: {
+      esmExternals: 'loose'
+    }
   };
   
-  module.exports = nextConfig;
+  export default nextConfig;
